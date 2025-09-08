f4b6412b5877e27b5701acc2c2ddc0b69f60c83d
licteo <134066234+licteo@users.noreply.github.com>
2025-09-08 07:55:36 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,46 @@
 const CACHE_NAME = 'iptv-player-cache-v1';
 const urlsToCache = [
-    './',
-    './iptv_player_pwa.html',
-    'https://cdn.jsdelivr.net/npm/hls.js@latest',
-    'https://cdn.tailwindcss.com',
-    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css',
-    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/svgs/solid/play-circle.svg'
+'./',
+'./iptv_player_pwa.html',
+'./manifest.json',
+'https://www.google.com/search?q=https://cdn.jsdelivr.net/npm/hls.js%40latest',
+'https://www.google.com/search?q=https://cdn.tailwindcss.com',
+'https://www.google.com/search?q=https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css'
 ];
 
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                console.log('Opened cache');
-                return cache.addAll(urlsToCache);
-            })
-    );
+self.addEventListener('install', (event) => {
+event.waitUntil(
+caches.open(CACHE_NAME)
+.then((cache) => {
+console.log('Opened cache');
+return cache.addAll(urlsToCache);
+})
+);
 });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
-            })
-    );
+self.addEventListener('fetch', (event) => {
+event.respondWith(
+caches.match(event.request)
+.then((response) => {
+if (response) {
+return response;
+}
+return fetch(event.request);
+})
+);
+});
+
+self.addEventListener('activate', (event) => {
+const cacheWhitelist = [CACHE_NAME];
+event.waitUntil(
+caches.keys().then((cacheNames) => {
+return Promise.all(
+cacheNames.map((cacheName) => {
+if (cacheWhitelist.indexOf(cacheName) === -1) {
+return caches.delete(cacheName);
+}
+})
+);
+})
+);
 });
